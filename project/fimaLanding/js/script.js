b951d937df2567bdd1ec591626e77b6826dfd981
Альберт <pythonyy@yandex.ru>
2019-12-23 23:37:43 +0500

    to add a new project and changed the fonts

--- project/fimaLanding/js/script.js
+++
@@ -1,7 +0,0 @@
-let burgerMenu = document.getElementById('burger-menu');
-let overlay = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
