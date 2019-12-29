05c8655049d94a113df2c8d2f1a6525a2ae6f167
Альберт <pythonyy@yandex.ru>
2019-12-29 09:58:34 +0500

    finished the project summerFruit

--- project/summerFruit/js/script.js
+++ project/summerFruit/js/script.js
@@ -1,5 +1,15 @@
-$('.bg').mousemove(function(e){
-	let movedX = (e.pageX * -1 / 50);
-	let movedY = (e.pageY * -1 / 100);
-	$(this).css('background-position', movedX + 'px ' + movedY + 'px');
+$(document).ready(function() {
+	// функция движения background'a, если ширина экрана > n px;
+  if ($(window).width() > 1440) {
+  	// изменение background'а при движении мыши;
+  	$('.backgoundg-mousemove').mousemove(function(event) {
+  		let movedX = (event.pageX * -1 / 50);
+  		let movedY = (event.pageY * -1 / 100);
+  		$(this).css('background-position', movedX + 'px ' + movedY + 'px');
+  	});
+  };
+  // адаптивное меню;
+  $('.adaptive-menu i').on('click', function() {
+  	$('.navigation').toggleClass('mobile-menu');
+  });
 });
