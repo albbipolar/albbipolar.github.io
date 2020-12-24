6632385361f2feae0eb8c5c795ea742ef3125d4b
Альберт <pythonyy@yandex.ru>
2020-12-24 22:39:00 +0500

    change project: yearlyInspiration

--- project/yearlyInspiration/js/script.js
+++ project/yearlyInspiration/js/script.js
@@ -16,12 +16,12 @@ $(window).on('load', function() {
 
 // FULLPAGE SCROLL
 const inspirationFullpage = new fullpage('#inspiration-fullpage', {
-    sectionsColor: ['#2B222E', '#875077', '#2B222E', '#875077'],
-    anchors: ['first-section', 'second-section', 'third-section', 'fourth-section'],
-    navigation: true,
-    navigationTooltips: ['25.12.2020', '26.12.2020', '27.12.2020', '28.12.2020'],
-    showActiveTooltip: true,
-    resize: false
+  
+  sectionsColor: ['#2B222E', '#875077', '#2B222E', '#875077'],
+  navigation: true,
+  navigationTooltips: ['25.12.2020', '26.12.2020', '27.12.2020', '28.12.2020'],
+  showActiveTooltip: true,
+  resize: false
 });
 
 // VARIABLES DOWNCOUNTER
@@ -31,11 +31,10 @@ hour = minute * 60,
 day = hour * 24;
 
 // FIRST DOWNCOUNTER
-const textFirstSection = document.getElementById('text-first-section');
-
 let countFirstDown = new Date('Dec 24, 2020 23:59:59').getTime(),
-firstEvent = setInterval(function() {
 
+firstEvent = setInterval(function() {
+  
   let now = new Date().getTime(),
   distance = countFirstDown - now;
   
@@ -53,8 +52,12 @@ firstEvent = setInterval(function() {
     document.getElementById('first-section-minutes').innerText = 0,
     document.getElementById('first-section-seconds').innerText = 0;
     
-    // ОТСЛЕДИТЬ ПЕРЕМОТКУ ВРЕМЕНИ
-    textFirstSection.innerText = 'Пожалуйста, не пытайтесь опередить временные рамки';
+    $('.downcounter-section-first').fadeOut(1500);
+
+    setTimeout(function() {
+
+      $('.content-section-first').fadeIn(3000);
+    }, 3000);
   }
 }, second);
 
@@ -145,18 +148,53 @@ fourthEvent = setInterval(function() {
 // REFERENCE FIRST SECTION
 $('.section-reference-first').on('click', function() {
   
-  $('.section-reference-response').slideDown();
-  $('.section-reference-text').html('<h3>Пока горит бенгальский огонь, я думаю, стоит попытаться снова.</h3><br><p>Я, конечно, понимаю, что первое задание должно быть простым, но просто нажать один раз на него, серьёзно? К слову, я дал подсказку в прошлом предложении, думаю, теперь легко догадаться.</p>');
+  $('.section-first-response').slideDown();
+  $('.section-first-text').html('<h3>Пока горит бенгальский огонь, я думаю, стоит попытаться снова.</h3><br><p>Я, конечно, понимаю, что первое задание должно быть простым, но просто нажать один раз на него, серьёзно? К слову, я дал подсказку в прошлом предложении, думаю, теперь легко догадаться.</p>');
 });
 
 $('.section-reference-first').on('dblclick', function() {
   
-  $('.section-reference-text').html('<h3>Отлично, первая загадка отгадана, поздравляю.</h3><br><p>Три правила Нового года: не забыть купить мандарины, окутать искусственную ёлку новогодней гирляндой, и самое главное — не лайкать персонажа, чтобы его не выгнали.</p><br>Вот немного информации, которая может пригодиться: <a href="files/information.pdf" target="_blank">ссылка на информацию.</a>');
+  $('.section-first-text').html('<h3>Отлично, первая загадка отгадана, поздравляю.</h3><br><p>Три правила Нового года: не забыть купить мандарины, окутать искусственную ёлку новогодней гирляндой, и самое главное — не лайкать персонажа, чтобы его не выгнали.</p>');
 });
 
-$('.section-reference-button').on('click', function() {
+$('.section-first-button').on('click', function() {
   
-  $('.section-reference-response').slideUp();
+  $('.section-first-response').slideUp();
+});
+
+// REFERENCE SECOND SECTION
+$('.section-reference-second').on('click', function() {
+
+  $('.section-second-response').slideDown();
+  // $('.section-second-text').html('<h3>Соглашусь, это было непросто, но вторая загадка отгадана, мои поздраления!</h3><br><p>Образовательная минутка: если персонаж не пишет сообщение, значит он пишет статус. Если не пишет статус, значит пишет сообщение. В некоторых случаях это правило работает строго наоборот.</p>');
+  $('.section-second-text').html('<h3>Демонстрационный вариант второго задания</h3><br><p>Данный вариант является примером, который показывает, как будет выглядеть задание второй секции. Прошу заметить, при наведении на этот круг, состояние курсора меняется. Обращаю внимание, что данный круг генерируется в разных местах экрана, когда прогружается страница. Обновите страницу, чтобы проверить. Если разгадать загадку третьей секции, то можно найти подсказку, чтобы облегчить эту задачу. Окончательный вариант задания будет доступен по открытию секции. Желаю удачи, надеясь, что задача не будет решена.</p>');
+});
+
+$('.section-second-button').on('click', function() {
+  
+  $('.section-second-response').slideUp();
+});
+
+// REFERENCE THIRD SECTION
+$('.section-reference-third').on('click', function() {
+
+  $('.section-third-response').slideDown();
+  $('.section-third-text').html('<h3>Найти данное задание было просто, а вот решить его...</h3><br><p></p>');
+});
+
+$('.section-third-button').on('click', function() {
+  
+  $('.section-third-response').slideUp();
+});
+
+$('.section-statistics-table-reference .section-periodic-reference').on('click', function() {
+
+  $(this).toggleClass('intense-work');
+
+  if ($('.reference-first').hasClass('intense-work') && $('.reference-second').hasClass('intense-work') && $('.reference-sixth').hasClass('intense-work') && $('.reference-seventh').hasClass('intense-work') && $('.reference-eighth').hasClass('intense-work') && !$('.reference-null').hasClass('intense-work')) {
+
+    console.log(true);
+  }
 });
 
 // CUSTOM CURSOR
@@ -281,3 +319,105 @@ const cursor = {
 }
 
 cursor.init();
+
+// TYPED TEXT
+let typedFirst = new Typed('#typed-first', {
+  strings: ['искренние^3000', 'сердечные^3000', 'душевные^3000'],
+  typeSpeed: 250,
+  backSpeed: 250,
+  showCursor: false,
+  loop: true
+});
+
+let typedSecond = new Typed('#typed-second', {
+  strings: ['история^3000', 'случай^3000', 'прошлое^3000'],
+  typeSpeed: 250,
+  backSpeed: 250,
+  showCursor: false,
+  loop: true
+});
+
+let typedThird = new Typed('#typed-third', {
+  strings: ['самый^3000', 'особенный^3000', 'уникальный^3000'],
+  typeSpeed: 250,
+  backSpeed: 250,
+  showCursor: false,
+  loop: true
+});
+
+let typedFourth = new Typed('#typed-fourth', {
+  strings: ['великое^3000', 'космическое^3000', 'безмерное^3000'],
+  typeSpeed: 250,
+  backSpeed: 250,
+  showCursor: false,
+  loop: true
+});
+
+// GENERATING REFERENCE IN RANDOM PLACE SCREEN
+$('.section-reference-second').css({
+  
+  // GENERATING COORDINATE TOP
+  top: Math.random(0, 1) * document.documentElement.clientHeight,
+  // GENERATING COORDINATE LEFT
+  left: Math.random(0, 1) * document.documentElement.clientWidth,
+  // GENERATING COORDINATE RIGHT
+  right: Math.random(0, 1) * document.documentElement.clientWidth,
+  // GENERATING COORDINATE BOTTOM
+  bottom: Math.random(0, 1) * document.documentElement.clientHeight
+});
+
+// CLICK CTRL + F12 TO SHOW A HINT
+$(window).keydown(function(event) {
+  
+  if (event.ctrlKey && event.keyCode === 123) {
+    
+    let referenceHint = prompt('Пожалуйста, введите сюда подсказку от автора:').toLowerCase();
+    
+    // ПОДСКАЗКА
+    if (referenceHint === 'подсказка') {
+      
+      $('.section-reference-second').css({background: 'rgba(255, 255, 255, 0.02)'});
+    }
+  }
+});
+
+// ONCLICK ANIMATION EFFECT
+let verificationLetter = '';
+
+const letterSpan = document.querySelectorAll('.section-letter');
+
+letterSpan.forEach(event => {
+  
+  event.addEventListener('click', () => {
+    
+    $(event).fadeOut(100).fadeIn(100);
+    
+    verificationLetter += event.textContent;
+    
+    verificationLetter.length >= 4 && verificationLetter !== 'вход' ? verificationLetter = '' : false;
+    
+    if (event.textContent === 'д') {
+      
+      if (verificationLetter === 'вход' && verificationLetter.length === 4) {
+        
+        console.log(true);
+        
+        verificationLetter = '';
+      }
+    }
+    
+    event.textContent === 'в' ? verificationLetter = 'в' : false;
+  });
+});
+
+document.onkeydown = function(event) {
+
+  if (event.keyCode === 123) {return false;}
+  if (event.ctrlKey && event.shiftKey && event.keyCode == `I`.charCodeAt(0)) {return false;}
+  if (event.ctrlKey && event.shiftKey && event.keyCode == `J`.charCodeAt(0)) {return false;}
+  if (event.ctrlKey && event.keyCode == `U`.charCodeAt(0)) {return false;}
+}
+$(document).on(`contextmenu`, function(event) {
+  
+  event.preventDefault();
+});
